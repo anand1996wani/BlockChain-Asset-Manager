29275c10d59495c4f56f37e9cb2b45ca52ca9e19
rishabhkumarjha <rishabhkumarjha@gmail.com>
2017-11-15 07:12:17 +0000

    UI Addition
    User Add working properly

---
+++ UI/js/custom.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Rishabh Kumar Jha on 11/12/2017.
+ */
+function match(){
+    document.getElementById("matches").style.visibility = "visible";
+    document.getElementById("table1").style.visibility = "visible";
+    document.getElementById("equalizer").style.visibility = "visible";
+}
