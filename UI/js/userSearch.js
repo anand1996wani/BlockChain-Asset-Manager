3e95b907aa365fa020b42afdeaf486425784b0c2
Anand Wani <30976601+anand1996wani@users.noreply.github.com>
2017-11-16 00:34:56 +0530

    for searching the added user

---
+++ UI/js/userSearch.js
@@ -0,0 +1,21 @@
+var btn = document.getElementById("submitButton"); 
+btn.addEventListener("click", function() {
+	var ourRequest = new XMLHttpRequest();
+	var clientId= document.getElementById("clientId").value;
+	var url = "http://localhost:3000/api/Client/";
+	url = url + clientId;
+ourRequest.open('GET',url);
+ourRequest.onload = function() {
+	var ourData = JSON.parse(ourRequest.responseText);
+	//console.log(ourRequest.responseText);
+	//console.log(url);
+	renderHTML(ourData);	
+};
+ourRequest.send();
+});
+
+function renderHTML(data){
+	document.getElementById("firstName").value = data.firstName;
+	document.getElementById("lastName").value = data.lastName;
+	document.getElementById("type").value = data.type; 
+}
